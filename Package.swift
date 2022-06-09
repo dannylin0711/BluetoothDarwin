c2e5fd59e2ec8c6e52ec76f3f0be04929be9e33f
LatoWolf <43667063+dannylin0711@users.noreply.github.com>
2022-06-09 15:20:15 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,12 @@ import PackageDescription
 
 let package = Package(
     name: "BluetoothDarwin",
+    platforms: [
+        .iOS(.v12),
+        .tvOS(.v12),
+        .watchOS(.v5),
+        .macOS(.v10_15)
+    ],
     products: [
         .library(
             name: "BluetoothDarwin",
